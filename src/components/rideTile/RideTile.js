4592526a4edc14c0ec2eda40ef7ea0e8a87406a9
MCdeGroot <mathijsdegroot@live.nl>
2023-07-03 23:25:02 +0200

    complete styling of rideTile component

--- src/components/rideTile/RideTile.js
+++ src/components/rideTile/RideTile.js
@@ -1,50 +1,64 @@
 import React from "react";
 import Button from "../button/Button";
+import "./RideTile.css"
+import {ChatText} from "@phosphor-icons/react";
+import {DownloadSimple} from "@phosphor-icons/react";
+import {Export} from "@phosphor-icons/react";
+import {DotsThreeOutline} from "@phosphor-icons/react";
+
 
 function RideTile() {
     return (
         <>
-            <div>
-                <section>
+            <div className="ridetile-outer-wrapper">
+                <section className="ridetile-top-styling">
+                    <img src="../../assets/roadbike.svg" alt=""/>
                     <div>
-
-                        {/*logo*/}
-                        <div>
-
-                        </div>
-                        <Button>
-
-                        </Button>
-
+                        <h5>Mathijs de Groot</h5>
+                        <p>datum 01-05-2023</p>
                     </div>
-                    <div>
-                        {/*    scheidingslijn*/}
-                    </div>
-                    <div>
+                    <Button className='icon-button'>
+                        <DotsThreeOutline color="#1989AC" width='2rem' height='2rem'/>
+                    </Button>
+                </section>
+                <div className='separation-line-top'>
+                    {/*    scheidingslijn*/}
+                </div>
+                <section className='ridetile-middle-styling'>
+                    <div className='ride-data'>
                         <div>
                             {/*    Mathday*/}
                             <h4>Title</h4>
-                            <p></p>
+                            <p>eventjes vlammen!</p>
                         </div>
-                        <div>
+                        <div className='ride-data-items'>
+
                             <div>
                                 <p>35,0km</p>
-                                <h3>Afstand</h3>
-
-                                {/*    afstand etc*/}
+                                <h4>Afstand</h4>
+                            </div>
+                            <div className='separation-line-middle'></div>
+                            <div>
+                                <p>35,0km</p>
+                                <h4>Tijd</h4>
+                            </div>
+                            <div className='separation-line-middle'></div>
+                            <div>
+                                <p>35,0km</p>
+                                <h4>Gem. Vermogen</h4>
                             </div>
                         </div>
                     </div>
-                    <img src="" alt=""/>
+                    <img src="../../assets/foto-test.jpg" alt="foto" width="150px" heigth="150px"/>
                 </section>
-                <section>
+                <section className='ridetile-bottom-styling flex-row'>
                     <h2>BIKENAME</h2>
-                    <div>
-                        <Button></Button>
-                        <div className='separation-line'></div>
-                        <Button></Button>
-                        <div className='separation-line'></div>
-                        <Button></Button>
+                    <div className='flex-row'>
+                        <Button className='icon-button'><DownloadSimple width='2rem' height='2rem'/></Button>
+                        <div className='separation-line-bottom'></div>
+                        <Button className='icon-button'><Export width='2rem' height='2rem'/></Button>
+                        <div className='separation-line-bottom'></div>
+                        <Button className='icon-button'><ChatText width='2rem' height='2rem'/></Button>
                     </div>
 
                 </section>
