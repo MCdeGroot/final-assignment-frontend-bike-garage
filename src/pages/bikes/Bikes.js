295f2a5eaddcaf022589a5d4d1f547f753606279
MCdeGroot <mathijsdegroot@live.nl>
2023-07-05 14:08:22 +0200

    add datafetch to bike and test bikepage

--- src/pages/bikes/Bikes.js
+++ src/pages/bikes/Bikes.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import BikeTile from "../../components/bikeTile/BikeTile";
+import axios from "axios";
 
 function Bikes() {
+    const [bikes, setBikes] = useState([]);
+
+    //data ophalen van de bikes
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('http://localhost:8080/bikes')
+                setBikes(response.data);
+                console.log(response.data)
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+
     return (
         <>
             <main className='outer-container'>
                 <div className='inner-container'>
                     <h1>Hier zijn de bikes</h1>
-                    <BikeTile></BikeTile>
+                    {bikes.map((bike) => {
+                        return (
+                            <BikeTile
+                                key={bike.id}
+                                brand={bike.brand}
+                                model={bike.model}
+                                name={bike.name}
+                                totalDistanceDriven={bike.totalDistanceDriven}
+                            ></BikeTile>
+                        )
+                    })
+                    }
                 </div>
             </main>
         </>
