6d7b4fdd16c1ede2c53df57564dc4f0a2f8042bb
MCdeGroot <mathijsdegroot@live.nl>
2023-07-10 08:21:00 +0200

    create PasswordChange page

--- src/pages/auth/PasswordChange.js
+++ src/pages/auth/PasswordChange.js
@@ -5,11 +5,69 @@ import Button from '../../components/button/Button'
 import {useForm} from "react-hook-form";
 import FormInputField from "../../components/formInput/FormInputField";
 import axios from "axios";
-import {AuthContext} from "../../context/AuthContext";
 
 function PasswordChange(){
     const {register, handleSubmit, reset} = useForm();
-    const {isAuth} = useContext(AuthContext);
+
+    async function handleFormSubmit(data){
+        const storedToken = localStorage.getItem('token');
+        console.log(storedToken);
+        console.log(data);
+        try {
+            const response = await axios.put(`http://localhost:8080/updatepassword/${data.username}`, {
+                username: data.username,
+                newPassword: data.newPassword
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${storedToken}`
+                }
+
+            });
+            console.log(response);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <>
+            <main className='outer-container'>
+                <div className='inner-container authorization'>
+                    <form className='form' onSubmit={handleSubmit(handleFormSubmit)}>
+                        <article className='form-wrapper'>
+                            <div className='form-title'>
+                                <h3>Ready to change your password?</h3>
+                            </div>
+                            <div className='form-input-items'>
+                                <FormInputField
+                                    name="username"
+                                    label="Login"
+                                    type="text"
+                                    placeholder="Fill in your username"
+                                    register={register}
+                                />
+
+                                <FormInputField
+                                    name="newPassword"
+                                    label="New Password"
+                                    type="password"
+                                    placeholder="Enter new password"
+                                    register={register}
+                                />
+                            </div>
+                        </article>
+                        <Button type="submit"
+                                className='signin-button'>
+                            Reset!
+                        </Button>
+                    </form>
+
+
+                </div>
+            </main>
+        </>
+    );
 
 
 
