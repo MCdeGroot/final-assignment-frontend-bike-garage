6a311f22c0c620ab786c6713d9ac74c53078da66
MCdeGroot <mathijsdegroot@live.nl>
2023-07-13 08:13:37 +0200

    add setBikeIcon helper function to set icons based on BikeType

---
+++ src/helper/setBikeIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {ReactComponent as TimeTrialBike} from "../../assets/timetrial.svg"
+import {ReactComponent as RoadBike} from "../../assets/roadbike.svg"
+import {ReactComponent as MountainBike} from "../../assets/mtb.svg"
+
+export function setBikeIcon ({bikeType}) {
+    let bikeIcon;
+    switch (bikeType) {
+        case "TIMETRIAL":
+            bikeIcon = <TimeTrialBike />;
+            break;
+        case "ROAD":
+            bikeIcon = <RoadBike/>;
+            break;
+        case "MOUNTAIN":
+            bikeIcon = <MountainBike />;
+            break;
+    }
+
+    return bikeIcon;
+
+}
