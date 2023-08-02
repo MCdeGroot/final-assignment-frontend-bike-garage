17ecd5e645acea53f798522abf816e6b685d702a
MCdeGroot <mathijsdegroot@live.nl>
2023-08-02 10:14:22 +0200

    add errorhandler and messages to login page

--- src/helper/errorHandler.js
+++ src/helper/errorHandler.js
@@ -1,5 +1,8 @@
 export function errorHandler(e) {
-    if (e.response.status === 400) {
+    if( e.response.status === 400 && e.response.data.includes("Cannot find user ")){
+        return "Looks like you put in a wrong combination of username and password"
+    }
+    else if (e.response.status === 400) {
         return "Looks like something went wrong, please fill in the right data";
     } else if (e.response.status === 401) {
         return "Looks like you're not authorized to do this";
